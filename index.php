5c2f234306930262f1cfb37915a4108630ef6973
Vinicius Dalsasso <vin.dalsasso@gmail.com>
2020-08-25 01:08:41 -0300

    arquivos base

---
+++ index.php
@@ -0,0 +1,259 @@
+<html>
+    <head>
+        <base href="http://www.vaionline.com.br//">
+
+        <link href="http://www.vaionline.com.br//css/template.css?v=2.0" rel="stylesheet" media="all">
+        <link href="http://www.vaionline.com.br//css/bootstrap/css/bootstrap.css?v=2.0" rel="stylesheet" media="all">
+    </head>
+                    
+     <body id="home">
+        
+        <div>
+            <div class="topo divMenTp menuTpFix">
+                <div class="container" style="position: relative;">
+                    <div class="divLogoTp">
+                        <a href="index.php">
+                            <img src="img/vaionline.png">
+                        </a>
+                    </div>
+                            
+                    <div class="divMenTop">
+                        <nav class="navbar navbar-light" style="margin-bottom: 0px!important;">
+                            <div class="col-md-12">
+                                <div >
+                                    <button type="button" class="navbar-toggle collapsed btnMenCollapsed" style="border: solid 1px #009999;" data-toggle="collapse" data-target="#defaultNavbar1">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                        <span ></span>
+                                    </button> 
+                                </div>
+                            </div>
+
+                            <div class="collapse navbar-collapse divTotmenuTop" id="defaultNavbar1" style="text-align: right; float: right;">               
+                                <ul class="nav navbar-nav menuTop"><li class="men1">   <a href="javascript:scrollElement('#home')" title="Home">Home</a></li><li class="men2">   <a href="javascript:scrollElement('#caracMn')" title="Características">Características</a></li><li class="men3">   <a href="javascript:scrollElement('#galeMn')" title="Galeria">Galeria</a></li><li class="men4">   <a href="javascript:scrollElement('#prodMn')" title="Produtos">Produtos</a></li><li class="men7">   <a href="https://docs.google.com/forms/d/1attJYCoz1ocGN0rYkCZoYuzMyRdNhF7Jc-CNCMG5aDk" title="Briefing">Briefing</a></li><li class="men5">   <a href="javascript:scrollElement('#contMn')" title="Contato">Contato</a></li>  </ul> 
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+
+                <!--
+            </div>
+            <div class="bannerInni"><div class="bannerInniImg" style="background-image: url('arquivo_usu/banner/sgsites-huner-20190530-140915.png');" title="Lance novos tempos para sua atividade"></div>
+                <div class="bannerInniCont">
+                    <div class="bannerInniContEsp">
+                        <div class="bannerInniTit">Lance novos tempos para sua atividade</div>
+                        <div class="bannerInniDesc"><span class="ellip">O vaionline® é uma ferramenta que vai elevar a divulgação de sua marca com baixo investimento, tendo como objetivo criar sites responsivos com o seu estilo.</span></div>
+                    </div>
+
+                    <div class="bnrSoc">
+                        <a href="https://www.facebook.com/HunerTI/" target="_blank"><i class="fab fa-facebook"></i></a>
+                        <a href="https://www.linkedin.com/company/huner-ti-colaborativa/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                        <a href="https://www.instagram.com/explore/locations/353114748/huner-ti-colaborativa" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <a href="mailto:huner@huner.com.br"><i class="fas fa-envelope"></i></a>
+                    </div>
+                </div>  
+                <div class="bannerInnift"></div>
+
+                <script>
+                    $('.bannerInniDesc').ellipsis({ 
+                        responsive: true 
+                    });
+                </script>
+                
+            </div>  
+            <div class="conteudo"><div class="divCarcInni" id="caracMn">  
+                <div class="container">
+                    <div class="divTitInni">
+                        <div>vaionline®</div>
+                        Características
+                    </div>
+                    <div class="divCarcInniEsp">
+                        <div class="divCarcInniEspFoto" style="background-image: url('arquivo_usu/institucional/fotoprincipal/sgsites-huner-20190531-114114.png')"></div>
+                        <div class="divCarcInniEspTit">Responsividade</div>
+                        <div class="divCarcInniEspDesc">Sites responsivos, funcionais em computadores, tablets, smartphones. </div>
+                    </div>
+                    <div class="divCarcInniEsp">
+                        <div class="divCarcInniEspFoto" style="background-image: url('arquivo_usu/institucional/fotoprincipal/sgsites-huner-20190531-114154.png')"></div>
+                        <div class="divCarcInniEspTit">Ganho de tempo</div>
+                        <div class="divCarcInniEspDesc">Fácil e rápido de gerenciar o seu site, economizando seu tempo para outras necessidades.</div>
+                    </div>
+                    <div class="divCarcInniEsp">
+                        <div class="divCarcInniEspFoto" style="background-image: url('arquivo_usu/institucional/fotoprincipal/sgsites-huner-20190531-114236.png')"></div>
+                        <div class="divCarcInniEspTit">Personalizavel</div>
+                        <div class="divCarcInniEspDesc">Layouts com personalização completa de acordo com a identidade visual da sua empresa.</div>
+                    </div>  
+                </div>
+
+                <div class="divFtBcCont"></div>
+            </div>
+            <div class="divGalInni" id="galeMn">
+                <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+                <script type="text/javascript" src="js/owl.carousel.js"></script>
+                <div class="container">
+                    <div class="divTitInni">
+                        <div>GALERIA</div>
+                        Alguns Exemplos
+                    </div>
+                    <div class="owl-carousel owl-theme owl-loaded owl-drag"><div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(-950px, 0px, 0px); transition: all 0.25s ease 0s; width: 3167px;"><div class="owl-item cloned" style="width: 306.667px; margin-right: 10px;"><a href="https://www.gamapatobranco.org.br/" target="_blank">
+                            <div class="divGalInniEspImg" title="GAMA" style="background-image: url('arquivo_usu/banner/sgsites-huner-20190531-151902.png');"></div>
+                        </a>
+                    </div>
+                    <div class="owl-item cloned" style="width: 306.667px; margin-right: 10px;"><a href="http://huner.com.br/" target="_blank">
+                            <div class="divGalInniEspImg" title="Huner" style="background-image: url('arquivo_usu/banner/sgsites-huner-20200123-154054.png');"></div>
+                        </a>
+                    </div>
+                    <div class="owl-item cloned" style="width: 306.667px; margin-right: 10px;"><a href="http://www.remansodapedreira.com.br/" target="_blank">
+                            <div class="divGalInniEspImg" title="Remanso da Pedreira" style="background-image: url('arquivo_usu/banner/sgsites-huner-20200123-154406.png');"></div>
+                        </a>
+                    </div>
+                    <div class="owl-item active" style="width: 306.667px; margin-right: 10px;"><a href="https://www.afinanceiro.com.br/" target="_blank">
+                            <div class="divGalInniEspImg" title="AFinanceiro" style="background-image: url('arquivo_usu/banner/sgsites-huner-20200123-154434.png');"></div>
+                        </a>
+                    </div>
+                    <div class="owl-item active" style="width: 306.667px; margin-right: 10px;"><a href="https://www.gamapatobranco.org.br/" target="_blank">
+                            <div class="divGalInniEspImg" title="GAMA" style="background-image: url('arquivo_usu/banner/sgsites-huner-20190531-151902.png');"></div>
+                        </a>
+                    </div>
+                    <div class="owl-item active" style="width: 306.667px; margin-right: 10px;"><a href="http://huner.com.br/" target="_blank">
+                            <div class="divGalInniEspImg" title="Huner" style="background-image: url('arquivo_usu/banner/sgsites-huner-20200123-154054.png');"></div>
+                        </a>
+                    </div>
+                    <div class="owl-item" style="width: 306.667px; margin-right: 10px;"><a href="http://www.remansodapedreira.com.br/" target="_blank">
+                            <div class="divGalInniEspImg" title="Remanso da Pedreira" style="background-image: url('arquivo_usu/banner/sgsites-huner-20200123-154406.png');"></div>
+                        </a>
+                    </div>
+                    <div class="owl-item cloned" style="width: 306.667px; margin-right: 10px;"><a href="https://www.afinanceiro.com.br/" target="_blank">
+                            <div class="divGalInniEspImg" title="AFinanceiro" style="background-image: url('arquivo_usu/banner/sgsites-huner-20200123-154434.png');"></div>
+                        </a>
+                    </div>
+                    <div class="owl-item cloned" style="width: 306.667px; margin-right: 10px;"><a href="https://www.gamapatobranco.org.br/" target="_blank">
+                            <div class="divGalInniEspImg" title="GAMA" style="background-image: url('arquivo_usu/banner/sgsites-huner-20190531-151902.png');"></div>
+                        </a>
+                    </div>
+                    <div class="owl-item cloned" style="width: 306.667px; margin-right: 10px;"><a href="http://huner.com.br/" target="_blank">
+                            <div class="divGalInniEspImg" title="Huner" style="background-image: url('arquivo_usu/banner/sgsites-huner-20200123-154054.png');"></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="owl-nav disabled"><button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">‹</span></button><button type="button" role="presentation" class="owl-next"><span aria-label="Next">›</span></button></div><div class="owl-dots"><button role="button" class="owl-dot active"><span></span></button><button role="button" class="owl-dot"><span></span></button></div></div>  </div>
+
+                <script>
+                    $('.owl-carousel').owlCarousel({
+                        loop : true,
+                        margin : 10,
+                        autoplay : true,
+                        items : 3,
+                        responsive:{
+                            0: {
+                                items:1,
+                            },
+                            500: {
+                                items:3,
+                            }
+                        }
+                    })
+                </script>
+                <div class="divFtCzCont"></div>
+              </div><div class="divProdInni" id="prodMn"> 
+                <div class="container">
+                    <div class="divTitInni">
+                        <div>Huner Ti</div>
+                        Mais Produtos
+                    </div><a href="http://www.acessopub.com.br/selentidade.php" target="_blank">
+                        <div class="divProdEspImg" title="Acesso Público" style="background-image: url('arquivo_usu/banner/sgsites-huner-20200123-104708.png');"></div>
+                      </a><a href="https://www.afinanceiro.com.br/" target="_blank">
+                        <div class="divProdEspImg" title="AFinanceiro" style="background-image: url('arquivo_usu/banner/sgsites-huner-20200123-104657.png');"></div>
+                      </a><a href="http://www.bidbox.com.br/" target="_blank">
+                        <div class="divProdEspImg" title="BidBox" style="background-image: url('arquivo_usu/banner/sgsites-huner-20200123-104648.png');"></div>
+                      </a><a href="http://www.lexpub.com.br/" target="_blank">
+                        <div class="divProdEspImg" title="LexPub" style="background-image: url('arquivo_usu/banner/sgsites-huner-20200123-104635.png');"></div>
+                      </a><a href="http://huner.com.br/pagina/institucional/conteudo/21-huner----sistemas-on-demand/6/3-produtos/estab/1/" target="_blank">
+                        <div class="divProdEspImg" title="Huner OnDemand" style="background-image: url('arquivo_usu/banner/sgsites-huner-20200123-104626.png');"></div>
+                      </a><a href="http://www.ser360.com.br/" target="_blank">
+                        <div class="divProdEspImg" title="Ser360" style="background-image: url('arquivo_usu/banner/sgsites-huner-20200123-104617.png');"></div>
+                      </a><a href="https://www.foodxp.com.br/" target="_blank">
+                        <div class="divProdEspImg" title="FoodXP" style="background-image: url('arquivo_usu/banner/sgsites-huner-20190531-165029.png');"></div>
+                      </a>  </div>
+                <div class="divFtBcCont"></div>
+              </div><div class="divContInni" id="contMn">
+                <div class="divContInniFd">
+                    <div class="container">
+                        <div class="divTitInni">
+                            <div>vaionline®</div>
+                            Deixe uma Mensagem
+                        </div>          <div class="divContInniCps">
+                            <form name="form1" action="#contMn" method="post">
+                                <div class="divInpEsp">
+                                    <input type="text" name="name" class="inputText" required="required" value="" placeholder="*Seu nome">
+                                </div>
+
+                                <div class="divInpEsp50">
+                                    <input type="email" name="email" class="inputText" required="required" value="" placeholder="email@email.com.br">
+                                </div>
+
+                                <div class="divInpEsp50" style="float: right !important;">
+                                    <input type="text" name="telefone" id="telefone" class="inputText" value="" placeholder="(99)9999-9999" maxlength="14" autocomplete="off">
+                                </div>
+
+                                <textarea name="descricao" class="txtArea" required="required" placeholder="*Deixe sua mensagem aqui..."></textarea>
+
+                                <input class="btEnvCont" type="submit" value="Enviar">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+              </div>  </div>  <div class="rodape">
+                    <div class="scrollup" onclick="scrollElement('#home');" style="display: block;">
+                        <i class="fa fa-angle-up"></i>
+                    </div>
+
+                    <div class="container">
+                        <div class="divFooterDesc">
+                            <div class="divLgFt">
+                                <a href="index.php">
+                                    <img src="img/vaionline.png">
+                                </a>
+                            </div>
+
+                        <div class="divFooterDescL">
+                            <div class="divEndFooter">
+                                <img src="img/local.png" class="iEndFooter">
+                                <div class="divEndFooterDsc">Rua Manoel Ribas, 665 - Vila Isabel | Pato Branco - PR | 85504-313
+                                </div>
+                            </div>
+                            
+                            <div class="divTelFooter">
+                                <img src="img/tel.png" class="iTelFooter">
+                                <div class="divTelFooterDsc">(46) 3225-7318
+                                </div>
+                            </div>
+
+                            <div class="divMailFooter">
+                                <img src="img/mail.png" class="iMailFooter">
+                                <div class="divMailFooterDsc">huner@huner.com.br
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="divLgHnr">
+                            <a href="http://www.huner.com.br" target="_blank">
+                                <div>Desenvolvido por:</div>
+                                <img src="img/logohnr.png">
+                            </a>
+                        </div>
+
+                        <div class="divFooterFb">
+                            <div id="fb-root" class=" fb_reset"><div style="position: absolute; top: -10000px; width: 0px; height: 0px;"><div></div></div></div>
+                            <script async="" defer="" crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&amp;version=v3.2"></script>
+                            <div class="fb-page fb_iframe_widget" data-href="https://www.facebook.com/HunerTI/" data-tabs="timeline" data-height="300" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" fb-xfbml-state="rendered" fb-iframe-plugin-query="adapt_container_width=true&amp;app_id=&amp;container_width=270&amp;height=300&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2FHunerTI%2F&amp;locale=pt_BR&amp;sdk=joey&amp;show_facepile=true&amp;small_header=true&amp;tabs=timeline"><span style="vertical-align: bottom; width: 270px; height: 300px;"><iframe name="f3050db8c249804" width="1000px" height="300px" data-testid="fb:page Facebook Social Plugin" title="fb:page Facebook Social Plugin" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" allow="encrypted-media" src="https://www.facebook.com/v3.2/plugins/page.php?adapt_container_width=true&amp;app_id=&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df148eca79a9b6d4%26domain%3Dwww.vaionline.com.br%26origin%3Dhttp%253A%252F%252Fwww.vaionline.com.br%252Ff22cc46d312fb24%26relation%3Dparent.parent&amp;container_width=270&amp;height=300&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2FHunerTI%2F&amp;locale=pt_BR&amp;sdk=joey&amp;show_facepile=true&amp;small_header=true&amp;tabs=timeline" style="border: none; visibility: visible; width: 270px; height: 300px;" class=""></iframe></span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            -->
+        </div>
+    </body>
+</html>
